dab7cad5754615214f197c5266dae9d6c5dc1ba3
Huanqing Li <hli122@uottawa.ca>
2015-11-24 22:52:12 -0500

    js-application

--- seamlessRolling/gallery.js
+++ seamlessRolling/gallery.js
@@ -1,3 +1,20 @@
 /**
  * Created by Muc on 15/11/24.
  */
+function gallery() {
+    var pic = document.getElementById("gallery");
+    var minPic = pic.getElementsByTagName("li");
+    for (var i = 0; i < minPic.length; i++) {
+        minPic[i].firstChild.onclick = function () {
+            replace(this);
+        }
+    }
+    function replace(img) {
+        var bigPic = document.getElementById("bigpic");
+        bigPic.src = img.src;
+        var picLink = document.getElementById("piclink");
+        picLink.href = img.src;
+    }
+    var oUl = document.getElementById("roller");
+    roller(oUl,pic,3,1)
+}gallery();
